bfade7b1f70ad057b5b480fe332b9c6baed79fe5
DinhMing <nguyendinhminh1810@gmail.com>
2023-07-03 17:07:27 +0700

    nhaptentuoi

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("phat trien phan men quan li");
+Console.WriteLine("Nhap ten cua ban: ");
+string name = Console.ReadLine();
+Console.WriteLine("Nhap tuoi: ");
+string tuoi = Console.ReadLine();
+Console.WriteLine("Sinh vien: " + name + " Co tuoi la: " + tuoi); 
+
+
+
